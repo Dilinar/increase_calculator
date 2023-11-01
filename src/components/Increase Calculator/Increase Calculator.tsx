f582aada3257b3abedaac59c762604e5858ee3e4
Szymon Jergas <szymonjergas@interia.pl>
2023-11-01 17:29:35 +0100

    Updated for more kinds of increases

--- src/components/Increase Calculator/Increase Calculator.tsx
+++ src/components/Increase Calculator/Increase Calculator.tsx
@@ -42,30 +42,48 @@ export function IncreaseCalculator() {
 
     const [ rowCount, setRowCount ] = useState(0);
     const [ increaseCount, setIncreaseCount ] = useState(0);
-    const [ tiemsToIncreaseInOther, setTiemsToIncreaseInOther ] = useState(0);
-    const [ timesToIncreaseInEach, setTimesToIncreaseInEach ] = useState(0);
+    const [ stsPerRow, setStsPerRow ] = useState(0);
+    const [ tiemsToIncre, setTiemsToIncrease ] = useState({ 1: 0, 2: 0 });
+    const [ increaseDistance, setIncreaseDistance ] = useState({ 1: 0, 2: 0 });
+    const [ rows, setRows ] = useState({ 1: 0, 2: 0 });
     const [ myRowCountError, setMyRowCountError ] = useState('');
     const [ myIncreaseCountError, setMyIncreaseCountError ] = useState('');
 
     function onSubmit (e: React.FormEvent) {
-
         e.preventDefault();
-        const rowCountHalf = rowCount / 2;
+        const increase = increaseCount / stsPerRow;
+        const baseIncrease = Math.trunc(rowCount / increase);
 
         if (isNaN(rowCount) || isNaN(increaseCount)) {
             return;
         }
-        if (rowCount % 2 > 0) {
-            setTiemsToIncreaseInOther(Math.floor(rowCountHalf) - (increaseCount - Math.ceil(rowCountHalf)));
-            setTimesToIncreaseInEach(increaseCount - (rowCountHalf - (increaseCount - rowCountHalf)));  
+        else if(rows[1] === 0 || rows[2] === 0 || isNaN(rows[1]) || isNaN(rows[2])) {
+            setTiemsToIncrease({
+                1: increase - (rowCount - (increase * baseIncrease)),
+                2: rowCount - (increase * baseIncrease)
+            });
+    
+            setIncreaseDistance({
+                1: baseIncrease,
+                2: baseIncrease + 1
+            });
         }
-        else 
-            setTiemsToIncreaseInOther(rowCountHalf - (increaseCount - rowCountHalf));
-        setTimesToIncreaseInEach(increaseCount - (rowCountHalf - (increaseCount - rowCountHalf)));
+        else {
+            setTiemsToIncrease({
+                1: increase - (rowCount - (increase * rows[1])) / (rows[2] - rows[1]),
+                2: (rowCount - (increase * rows[1])) / (rows[2] - rows[1])
+            });
+    
+            setIncreaseDistance({
+                1: rows[1],
+                2: rows[2]
+            });
+        }
+
     }
 
     function onRowCountChange (e: React.ChangeEvent<HTMLInputElement>) {
-        const value = parseInt(e.target.value, 10);
+        const value = parseInt(e.target.value);
 
         if (isNaN(value)) {
             setMyRowCountError("Field required.");
@@ -76,7 +94,7 @@ export function IncreaseCalculator() {
     }
 
     function onIncreaseCountChange (e: React.ChangeEvent<HTMLInputElement>) {
-        const value = parseInt(e.target.value, 10);
+        const value = parseInt(e.target.value);
 
         if (isNaN(value)) {
             setMyIncreaseCountError("Field required.");
@@ -86,6 +104,31 @@ export function IncreaseCalculator() {
         setIncreaseCount(value);
     }
 
+    function onStsPerRowChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = parseInt(e.target.value);
+
+        if (isNaN(value)) {
+            setMyIncreaseCountError("Field required.");
+        }
+        else setMyIncreaseCountError('');
+
+        setStsPerRow(value);
+    }
+
+    const onSetRowsChange = (num: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value);
+
+        if (isNaN(value)) {
+            setMyIncreaseCountError("Field required.");
+        }
+        else setMyIncreaseCountError('');
+
+        setRows({
+            ...rows,
+            [num]: value
+        });
+    };
+    
     return (
         <div className={classes.root}>
             <h1>Increase Calculator</h1>
@@ -111,6 +154,30 @@ export function IncreaseCalculator() {
                         helperText={myIncreaseCountError}
                         onChange={onIncreaseCountChange}
                     />
+                    <TextField className={classes.textField}
+                        label='Number of stitches per row'
+                        type='number'
+                        variant='outlined'
+                        value={stsPerRow}
+                        InputProps={{ inputProps: { min: 1 } }}
+                        error={!!myIncreaseCountError}
+                        helperText={myIncreaseCountError}
+                        onChange={onStsPerRowChange}
+                    />
+                    <TextField className={classes.textField}
+                        label='First increase every'
+                        type='number'
+                        variant='outlined'
+                        value={rows[1]}
+                        onChange={onSetRowsChange(1)}
+                    />
+                    <TextField className={classes.textField}
+                        label='Then increase every'
+                        type='number'
+                        variant='outlined'
+                        value={rows[2]}
+                        onChange={onSetRowsChange(2)}
+                    />
                 </div>
                 <div className={classes.buttonContainer}>
                     {isNaN(rowCount) || isNaN(increaseCount) || rowCount < 1 || increaseCount < 1 ?
@@ -131,8 +198,14 @@ export function IncreaseCalculator() {
                 </div>
             </form>
             <div className={classes.resultContainer}>
-                <span>Increse in every other row {tiemsToIncreaseInOther} times.</span>
-                <span>Increase in each row {timesToIncreaseInEach} times.</span>
+                {tiemsToIncre[1] === 0 ?
+                    null
+                    : <span>Increse {tiemsToIncre[1]} {tiemsToIncre[1] === 1 ? "time" : "times"} in every {increaseDistance[1] === 1 ? '' : increaseDistance[1] === 2 ? `${increaseDistance[1]}nd` : increaseDistance[1] === 3 ? `${increaseDistance[1]}rd` : `${increaseDistance[1]}th`} row. </span>
+                }    
+                {tiemsToIncre[2] === 0 ?
+                    null
+                    : <span>Increse {tiemsToIncre[2]} {tiemsToIncre[2] === 1 ? "time" : "times"} in every {increaseDistance[2] === 2 ? `${increaseDistance[2]}nd` : increaseDistance[2] === 3 ? `${increaseDistance[2]}rd` : `${increaseDistance[2]}th`} row.</span>
+                }   
             </div>
         </div>
     );
