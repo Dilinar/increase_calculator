4af95061c9ac0751d1eb8063f17afb7d77c2aa68
Dilinar <szymonjergas@interia.pl>
2023-12-17 10:52:17 +0100

    Updated the mathrmatical formulas.

--- src/components/Increase Calculator/Increase Calculator.tsx
+++ src/components/Increase Calculator/Increase Calculator.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import {
     Button,
     TextField,
+    Switch,
+    FormControlLabel,
     makeStyles,
 } from '@material-ui/core';
 
@@ -19,20 +21,25 @@ const useStyles = makeStyles((theme) => ({
     },
     formInputs: {
         display: 'flex',
-        flexFlow: 'column wrap'
+        flexFlow: 'column wrap',
     },
     textField: {
-        margin: theme.spacing(2)
+        margin: theme.spacing(2),
+        width: '300px',
     },
     buttonContainer: {
         display: 'flex',
         flexFlow: 'column wrap',
+        marginTop: theme.spacing(2)
     },
     resultContainer: {
         display: 'flex',
         flexFlow: 'column wrap',
-        marginTop: theme.spacing(2),
+        marginTop: theme.spacing(5),
         fontSize: '1.5rem'
+    },
+    h3: {
+        marginBottom: theme.spacing(2)
     }
 }));
 
@@ -40,14 +47,14 @@ export function IncreaseCalculator() {
 
     const classes = useStyles();
 
-    const [ rowCount, setRowCount ] = useState(0);
-    const [ increaseCount, setIncreaseCount ] = useState(0);
-    const [ stsPerRow, setStsPerRow ] = useState(0);
-    const [ tiemsToIncre, setTiemsToIncrease ] = useState({ 1: 0, 2: 0 });
+    const [ rowCount, setRowCount ] = useState(1);
+    const [ increaseCount, setIncreaseCount ] = useState(1);
+    const [ stsPerRow, setStsPerRow ] = useState(1);
+    const [ tiemsToIncrease, setTiemsToIncrease ] = useState({ 1: 0, 2: 0 });
     const [ increaseDistance, setIncreaseDistance ] = useState({ 1: 0, 2: 0 });
-    const [ rows, setRows ] = useState({ 1: 0, 2: 0 });
-    const [ myRowCountError, setMyRowCountError ] = useState('');
-    const [ myIncreaseCountError, setMyIncreaseCountError ] = useState('');
+    const [ distRows, setDistRows ] = useState({ 1: 1, 2: 2 });
+    const [ myFieldErrors, setMyFieldErrors ] = useState({ 1: "", 2: "", 3: "", 4: "", 5: "" });
+    const [ showAdvancedSettings, setShowAdvancedSettings ] = useState(false);
 
     function onSubmit (e: React.FormEvent) {
         e.preventDefault();
@@ -57,7 +64,7 @@ export function IncreaseCalculator() {
         if (isNaN(rowCount) || isNaN(increaseCount)) {
             return;
         }
-        else if(rows[1] === 0 || rows[2] === 0 || isNaN(rows[1]) || isNaN(rows[2])) {
+        else if(distRows[1] === 0 || distRows[2] === 0 || isNaN(distRows[1]) || isNaN(distRows[2]) || showAdvancedSettings === false) {
             setTiemsToIncrease({
                 1: increase - (rowCount - (increase * baseIncrease)),
                 2: rowCount - (increase * baseIncrease)
@@ -65,69 +72,115 @@ export function IncreaseCalculator() {
     
             setIncreaseDistance({
                 1: baseIncrease,
-                2: baseIncrease + 1
+                2: baseIncrease <= 0 ? 0 : baseIncrease + 1
             });
         }
-        else {
-            setTiemsToIncrease({
-                1: increase - (rowCount - (increase * rows[1])) / (rows[2] - rows[1]),
-                2: (rowCount - (increase * rows[1])) / (rows[2] - rows[1])
+        else if (showAdvancedSettings === true) {
+            if (distRows[1] === distRows[2]) {
+                setMyFieldErrors({
+                    ...myFieldErrors,
+                    4: "values can not be equal",
+                    5: "values can not be equal"
+                });
+            }
+            else {
+                setTiemsToIncrease({
+                    1: increase - (rowCount - (increase * distRows[1])) / (distRows[2] - distRows[1]),
+                    2: (rowCount - (increase * distRows[1])) / (distRows[2] - distRows[1])
+                });
+        
+                setIncreaseDistance({
+                    1: distRows[1],
+                    2: baseIncrease <= 0 ? 0 : distRows[2]
+                });
+            }
+        }
+
+    }
+
+    function getOrdinal (num: number) {
+        if (num === 1 ) {
+            return '';
+        }
+
+        if (num % 10 === 1 && num % 100 !== 11) {
+            return `${num}st`;
+        }
+     
+        if (num % 10 === 2 && num % 100 !== 12) {
+            return `${num}nd`;
+        }
+     
+        if (num % 10 === 3 && num % 100 !== 13) {
+            return `${num}rd`;
+        }
+     
+        return `${num}th`;
+    }
+ 
+    function getResultMessage (mult: number, dist: number, num: number) {
+        if (mult <= 0 || dist <= 0 || mult === Infinity || dist === Infinity || tiemsToIncrease[1] * increaseDistance[1] > rowCount) {
+            if (num === 1) {
+                return "Impossible to calculate increases, the provided values are incorrect.";
+            }
+            else return "";
+        }
+        else return `Increase ${mult} time${mult > 1 ? 's' : ''} in every ${getOrdinal(dist)} row.`;
+    }
+
+    function onChangeValue (setter: any, num: number) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            const value = parseInt(e.target.value);
+
+            if (isNaN(value)) {
+                setMyFieldErrors({
+                    ...myFieldErrors,
+                    [num]: "Field required."
+                });
+            }
+            else setMyFieldErrors({
+                ...myFieldErrors,
+                [num]: ""
             });
     
-            setIncreaseDistance({
-                1: rows[1],
-                2: rows[2]
+            setter(value);
+        };
+    }
+
+    function onChangeObjectValue (num: number, errNum: number,) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            const value = parseInt(e.target.value);
+
+            if (isNaN(value) || value <= 0 || distRows[num] <= 0) {
+                setMyFieldErrors({
+                    ...myFieldErrors,
+                    [errNum]: "Insert value > 0"
+                });
+            }
+            else setMyFieldErrors({
+                ...myFieldErrors,
+                [errNum]: ""
             });
-        }
-
+    
+            setDistRows({
+                ...distRows,
+                [num]: value
+            });
+        };
     }
 
-    function onRowCountChange (e: React.ChangeEvent<HTMLInputElement>) {
-        const value = parseInt(e.target.value);
-
-        if (isNaN(value)) {
-            setMyRowCountError("Field required.");
-        }
-        else setMyRowCountError('');
-
-        setRowCount(value);
+    function switchAdvancedSettings (event: React.ChangeEvent<HTMLInputElement>) {
+        setShowAdvancedSettings(event.target.checked );
     }
 
-    function onIncreaseCountChange (e: React.ChangeEvent<HTMLInputElement>) {
-        const value = parseInt(e.target.value);
-
-        if (isNaN(value)) {
-            setMyIncreaseCountError("Field required.");
+    function buttonDisable () {
+        if ((isNaN(rowCount) || isNaN(increaseCount) || isNaN(stsPerRow) || rowCount < 1 || increaseCount < 1 || stsPerRow < 1) ||
+            ((isNaN(distRows[1]) || isNaN(distRows[2]) || distRows[1] < 1 || distRows[2] < 1) && showAdvancedSettings === true)) {
+            return true;
         }
-        else setMyIncreaseCountError('');
-
-        setIncreaseCount(value);
+        else return false;
+        
     }
-
-    function onStsPerRowChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const value = parseInt(e.target.value);
-
-        if (isNaN(value)) {
-            setMyIncreaseCountError("Field required.");
-        }
-        else setMyIncreaseCountError('');
-
-        setStsPerRow(value);
-    }
-
-    const onSetRowsChange = (num: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(e.target.value);
-
-        if (isNaN(value)) {
-            setMyIncreaseCountError("Field required.");
-        }
-        else setMyIncreaseCountError('');
-
-        setRows({
-            ...rows,
-            [num]: value
-        });
-    };
     
     return (
         <div className={classes.root}>
@@ -135,77 +188,93 @@ export function IncreaseCalculator() {
             <form className={classes.form} onSubmit={onSubmit}>
                 <div className={classes.formInputs}>
                     <TextField className={classes.textField}
-                        label='Number of rows'
+                        label="Number of rows to knit"
                         type='number'
                         variant='outlined'
                         value={rowCount}
                         InputProps={{ inputProps: { min: 1 } }}
-                        error={!!myRowCountError}
-                        helperText={myRowCountError}
-                        onChange={onRowCountChange}
+                        error={!!myFieldErrors[1]}
+                        helperText={myFieldErrors[1]}
+                        onChange={onChangeValue(setRowCount, 1)}
                     />
                     <TextField className={classes.textField}
-                        label='Number of stitches to encrease'
+                        label="Number of stitches to add"
                         type='number'
                         variant='outlined'
                         value={increaseCount}
                         InputProps={{ inputProps: { min: 1 } }}
-                        error={!!myIncreaseCountError}
-                        helperText={myIncreaseCountError}
-                        onChange={onIncreaseCountChange}
+                        error={!!myFieldErrors[2]}
+                        helperText={myFieldErrors[2]}
+                        onChange={onChangeValue(setIncreaseCount, 2)}
                     />
                     <TextField className={classes.textField}
-                        label='Number of stitches per row'
+                        label="Number of stitches to add per increase"
                         type='number'
                         variant='outlined'
                         value={stsPerRow}
                         InputProps={{ inputProps: { min: 1 } }}
-                        error={!!myIncreaseCountError}
-                        helperText={myIncreaseCountError}
-                        onChange={onStsPerRowChange}
+                        error={!!myFieldErrors[3]}
+                        helperText={myFieldErrors[3]}
+                        onChange={onChangeValue(setStsPerRow, 3)}
                     />
-                    <TextField className={classes.textField}
-                        label='First increase every'
-                        type='number'
-                        variant='outlined'
-                        value={rows[1]}
-                        onChange={onSetRowsChange(1)}
-                    />
-                    <TextField className={classes.textField}
-                        label='Then increase every'
-                        type='number'
-                        variant='outlined'
-                        value={rows[2]}
-                        onChange={onSetRowsChange(2)}
-                    />
+                    <div className={classes.form} >
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={showAdvancedSettings}
+                                    onChange={switchAdvancedSettings}
+                                    name="Advanced settings"
+                                    color='default'
+                                />
+                            }
+                            label="Toggle advanced settings"  
+                        />
+                    </div>
+                    
+                    {showAdvancedSettings === true ? 
+                        <section className={classes.form} >
+                            <h3 className={classes.h3}>
+                                Advanced Settings
+                            </h3>
+                            <p>Specify custom increase rows</p>
+                            <TextField className={classes.textField}
+                                label="First increase every"
+                                type='number'
+                                variant='outlined'
+                                value={distRows[1]}
+                                error={!!myFieldErrors[4]}
+                                helperText={myFieldErrors[4]}
+                                onChange={onChangeObjectValue(1, 4)}
+                            />
+                            <TextField className={classes.textField}
+                                label="Then increase every"
+                                type='number'
+                                variant='outlined'
+                                value={distRows[2]}
+                                error={!!myFieldErrors[5]}
+                                helperText={myFieldErrors[5]}
+                                onChange={onChangeObjectValue(2, 5)}
+                            />
+                        </section>
+                        : null
+                    }
                 </div>
                 <div className={classes.buttonContainer}>
-                    {isNaN(rowCount) || isNaN(increaseCount) || rowCount < 1 || increaseCount < 1 ?
-                        <Button
-                            disabled
-                            type='submit'
-                            variant='contained'
-                            color='primary'>
-                            Calculate
-                        </Button> :
-                        <Button
-                            type='submit'
-                            variant='contained'
-                            color='primary'>
-                            Calculate
-                        </Button>
-                    }
+                    <Button
+                        disabled={buttonDisable()}
+                        type='submit'
+                        variant='contained'
+                        color='primary'>
+                        Calculate
+                    </Button>
                 </div>
             </form>
             <div className={classes.resultContainer}>
-                {tiemsToIncre[1] === 0 ?
+                {tiemsToIncrease[1] === 0 ?
                     null
-                    : <span>Increse {tiemsToIncre[1]} {tiemsToIncre[1] === 1 ? "time" : "times"} in every {increaseDistance[1] === 1 ? '' : increaseDistance[1] === 2 ? `${increaseDistance[1]}nd` : increaseDistance[1] === 3 ? `${increaseDistance[1]}rd` : `${increaseDistance[1]}th`} row. </span>
+                    : <span>{getResultMessage(tiemsToIncrease[1], increaseDistance[1], 1)}</span>
                 }    
-                {tiemsToIncre[2] === 0 ?
-                    null
-                    : <span>Increse {tiemsToIncre[2]} {tiemsToIncre[2] === 1 ? "time" : "times"} in every {increaseDistance[2] === 2 ? `${increaseDistance[2]}nd` : increaseDistance[2] === 3 ? `${increaseDistance[2]}rd` : `${increaseDistance[2]}th`} row.</span>
-                }   
+                <span>{getResultMessage(tiemsToIncrease[2], increaseDistance[2], 2)}</span>
             </div>
         </div>
     );
